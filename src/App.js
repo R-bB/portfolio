7708e880c8f734a99a3decfb9ec4621f90afadec
r-b_b <r.boehme@outlook.com>
2019-04-07 22:55:47 -0500

    Added react-router-dom and added some pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import './App.css';
+import Routes from './components/routes';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
+      <div className="demo-big-content">
+          <Layout>
+              <Header title="Title" scroll>
+                  <Navigation>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/contact">Contact Me</Link>
+                  </Navigation>
+              </Header>
+              <Drawer title="Title">
+                  <Navigation>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/contact">Contact Me</Link>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+                  <Routes/>
+              </Content>
+          </Layout>
       </div>
+
     );
   }
 }
